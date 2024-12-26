bcb95d1d92e63157296855f19d2515493086827c
mohak <mohak.londhe@gmail.com>
2024-12-26 00:55:18 -0600

    adds skills section

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const Works = () => {
-  return <div>Works</div>;
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt={"github"}
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
 };
 
-export default Works;
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work?</p>
+        <h2 className={styles.sectionHeadText}>Skills</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Frontend Technologies: ReactJS (Redux / Jotai), React Native,
+          TypeScript (JavaScript ES2015), HTML & CSS (Bootstrap / Tailwind CSS),
+          Jest & Enzyme (Unit Testing Frameworks), JSON & XML Backend
+          Technologies: NodeJS (Express), .NET Databases: MongoDB, Redis,
+          PostgreSQL, MS SQL Server CI/CD Pipelines: Git, Docker
+          (Containerization), Jenkins (CI/CD) Version Control and Collaboration
+          Tools: GitHub, Microsoft Azure Soft Skills: Fluent Verbal / Written
+          Communication, Quick learner, Self-directed, Team Player
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((p, i) => (
+          <ProjectCard key={`project${i}`} index={i} {...p} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
